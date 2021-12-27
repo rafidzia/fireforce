60ecbc4b3cf5bcd7b21b757db955abb86cd61403
rafidzia <32030497+rafidzia@users.noreply.github.com>
2021-12-28 03:20:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -174,9 +174,9 @@ io.on('connection', function (socket) {
             if(result.length == 0) return;
             result = result[0];
             if(result.demand != "-"){
-                socket.emit("firemanStatusResult", {"status" : true})
+                socket.emit("firemanStatusResult", {"status" : true, "demand" : result.demand})
             }else{
-                socket.emit("firemanStatusResult", {"status" : false})
+                socket.emit("firemanStatusResult", {"status" : false, "demand" : result.demand})
             }
         })
     })
@@ -201,6 +201,29 @@ io.on('connection', function (socket) {
             }
             socket.emit("userDetailResult", records)
         })
+
+        socket.on("userSearchByFloorRoom", (data)=>{
+            data.token = crypto.createHash('sha256').update(data.token).digest('hex');
+            db.collection("user").find({"id" : data.id, "token" : data.token}).toArray((err, result)=>{
+                if(err) throw err;
+                if(result.length == 0) return;
+                result = result[0];
+                let records = {};
+                for(let key in result){
+                    if(key == data.floor){
+                        records.floor = key.substring(1, key.length);   
+                        for(let key1 in result[key]){
+                            if(key1 == data.room){
+                                records.room = result[key][key1][1];
+                            }
+                        }
+                    }
+                }
+                socket.emit("userSearchByFloorRoomResult", records)
+            })
+        })
+
+        // socket.
 
     })
 
